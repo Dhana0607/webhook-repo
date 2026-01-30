95c9cce8d80b696ba245914c575bf7f9c0ba3634
Dhana0607 <dhanalakshmi.r.2019.cse@rajalakshmi.edu.in>
2026-01-30 17:27:33 +0530

    chore: setup mongodb atlas connection

--- app.py
+++ app.py
@@ -3,15 +3,11 @@ from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+load_dotenv(dotenv_path=".env")
 app = Flask(__name__)
 
 #Mongo db setup
-MONGO_URI = os.getenv("MONGO_URI")
-if not MONGO_URI:
-    raise Exception("MONGO_URI not found in environment variables")
-
-client = MongoClient(MONGO_URI)
+client = MongoClient(os.getenv("MONGO_URI"))
 db = client.github_events
 collection = db.events
 
@@ -26,12 +22,6 @@ def webhook():
 @app.route("/events")
 def events():
     return jsonify([])
-
-@app.route("/test-db")
-def test_db():
-    collection.insert_one({"test": "atlas_connection"})
-    return "MongoDB Atlas connected!"
-
 
 if __name__ == "__main__" :
     app.run(debug=True)
